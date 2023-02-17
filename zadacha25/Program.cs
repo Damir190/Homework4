7fb94727bef4fce41d0ce81c36402cae5134101b
Damir <ag_cmg@mail.ru>
2023-02-17 18:21:49 +0300

    zad25

---
+++ zadacha25/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 25: Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Pow(int numberA, int numberB)
+{
+    int x = 1;
+
+
+    for (int i = 1; i <= numberB; i++)
+    {
+        x *= numberA;
+    }
+
+
+    return x;
+}
+
+void main()
+{
+    Console.Clear();
+    Console.Write("Введите число A: ");
+    int numA = int.Parse(Console.ReadLine()!);
+    Console.Write("Введите число B: ");
+    int numB = int.Parse(Console.ReadLine()!);
+    int result = Pow(numA, numB);
+    Console.WriteLine(result);
+}
+
+main();
